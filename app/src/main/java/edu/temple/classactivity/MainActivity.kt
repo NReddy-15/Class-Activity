16451953504271a8f99e33ac8f499f6b9c62c3a4
Nandita <129239412+NReddy-15@users.noreply.github.com>
2025-01-28 12:15:04 -0500

    Added text change functionality to button text with click

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,24 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById(R.id.clickMeButton) as Button).setOnClickListener {
+            (it as Button).text = "Button Clicked"
+        }
+
     }
+
+
 }
